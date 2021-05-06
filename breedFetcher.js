bfb6bade46b5516d2f9ab9f60419408ef341f4f5
Matt <mattluo604@hotmail.com>
2021-05-06 22:13:10 +0000

    breed fetch function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,21 @@
 const request = require("request");
-const args = process.argv.slice(2);
 
-const breedFetcher = (args) => {
+
+const fetchBreedDescription = (breedName,callback) => {
   request(
-    `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`,
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
     (error, response, body) => {
       if (error) {
-        console.log("error:", error);
-        console.log("statusCode:", response && response.statusCode);
-        return;
+        callback(error,null);
       }
       if (body === []) {
         return console.log("This type of breed is not found");
       }
       const data = JSON.parse(body);
-      console.log(data[0].description);
+      callback(null,data[0].description);
     }
   );
 };
-breedFetcher(args);
+
+
+module.exports = fetchBreedDescription;
